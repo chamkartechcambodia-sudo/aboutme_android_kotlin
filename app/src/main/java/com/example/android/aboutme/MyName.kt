acf6ec23081396ab964b15785d63fbd7c82cdc6f
chamkartechcambodia-sudo <chamkartechcambodia@gmail.com>
2026-05-04 12:27:53 +0700

    chore: Step.02-Exercise-Add-TextView-ImageView-Style

---
+++ app/src/main/java/com/example/android/aboutme/MyName.kt
@@ -0,0 +1,8 @@
+package com.example.android.aboutme
+
+/**
+ * Container for a name and a nickname strings.
+ * Used to demonstrate data binding.
+ */
+
+data class MyName(var name: String = "", var nickname: String = "")
