11c567ca048a73d0e2f335aec59b5302467e0da6
chamkartechcambodia-sudo <chamkartechcambodia@gmail.com>
2026-05-04 12:28:10 +0700

    chore: Step.05-Solution-Implement-data-binding

---
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.android.aboutme
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * Main Activity of the AboutMe app. This app demonstrates:
+ *     * LinearLayout with TextViews, ImageView, Button, EditText, and ScrollView
+ *     * ScrollView to display scrollable text
+ *     * Getting user input with an EditText.
+ *     * Click handler for a Button to retrieve text from an EditText and set it in a TextView.
+ *     * Setting the visibility status of a view.
+ *     * Data binding between MainActivity and activity_main.xml. How to remove findViewById,
+ *       and how to display data in views using the data binding object.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
